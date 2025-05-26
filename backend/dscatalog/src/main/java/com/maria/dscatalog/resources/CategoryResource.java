fcbe2f8bdabf179986f95f1770732ff565c5abb7
MariaHRCastro <mariahribeiro@protonmail.com>
2025-05-25 21:13:25 -0300

    Método insert

--- backend/dscatalog/src/main/java/com/maria/dscatalog/resources/CategoryResource.java
+++ backend/dscatalog/src/main/java/com/maria/dscatalog/resources/CategoryResource.java
@@ -1,12 +1,18 @@
 package com.maria.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.maria.dscatalog.dto.CategoryDTO;
 import com.maria.dscatalog.entities.Category;
@@ -23,5 +29,18 @@ public class CategoryResource {
 	public ResponseEntity<List<CategoryDTO>> findAll(){
 		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		CategoryDTO catDto = service.findById(id);
+		return ResponseEntity.ok().body(catDto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- backend/dscatalog/src/main/java/com/maria/dscatalog/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/maria/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.maria.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,19 +11,36 @@ import org.springframework.transaction.annotation.Transactional;
 import com.maria.dscatalog.dto.CategoryDTO;
 import com.maria.dscatalog.entities.Category;
 import com.maria.dscatalog.repositories.CategoryRepository;
+import com.maria.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
+	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();
 		List<CategoryDTO> listDto = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-		
+
 		return listDto;
 	}
+
+	@Transactional
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category cat = obj.orElseThrow(() -> new EntityNotFoundException("A categoria "+id+" não existe!"));
+		return new CategoryDTO(cat);
+	}
 	
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
+	
+
 }
